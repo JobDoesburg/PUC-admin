a76a140ceaa5e122a26d2d51d0946e7ea1fbc163
Job Doesburg <jdoesburg@lilo7.science.ru.nl>
2021-07-14 19:58:09 +0200

    prepare for production

--- PUCadmin/PUCadmin/admin.py
+++
@@ -1,4 +0,0 @@
-from django.contrib import admin
-
-admin.site.site_header = "PUC admin"
-admin.site.site_title = "PUC admin"
--- PUCadmin/schools/resources.py
+++
@@ -1,9 +0,0 @@
-from import_export import resources
-
-from .models import School
-
-
-class SchoolResource(resources.ModelResource):
-    class Meta:
-        model = School
-        exclude = ()
