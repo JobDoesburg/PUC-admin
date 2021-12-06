7d5b74cdbd2c4dd74b14e78e48a7253e28495d37
Job Doesburg <j.j.doesburg@student.ru.nl>
2021-12-06 17:42:34 +0100

    Fix settings

--- pucadmin/PUCadmin/settings/base.py
+++ pucadmin/PUCadmin/settings/base.py
@@ -123,23 +123,3 @@ PRIVACY_STATEMENT_URL = (
 
 TAGGIT_TAGS_FROM_STRING = "taggit_selectize.utils.parse_tags"
 TAGGIT_STRING_FROM_TAGS = "taggit_selectize.utils.join_tags"
-TAGGIT_SELECTIZE = {
-    "MINIMUM_QUERY_LENGTH": 2,
-    "RECOMMENDATION_LIMIT": 10,
-    "CSS_FILENAMES": ("taggit_selectize/css/selectize.django.css",),
-    "JS_FILENAMES": ("taggit_selectize/js/selectize.js",),
-    "DIACRITICS": True,
-    "CREATE": True,
-    "PERSIST": True,
-    "OPEN_ON_FOCUS": True,
-    "HIDE_SELECTED": True,
-    "CLOSE_AFTER_SELECT": False,
-    "LOAD_THROTTLE": 300,
-    "PRELOAD": False,
-    "ADD_PRECEDENCE": False,
-    "SELECT_ON_TAB": False,
-    "REMOVE_BUTTON": False,
-    "RESTORE_ON_BACKSPACE": False,
-    "DRAG_DROP": False,
-    "DELIMITER": ",",
-}
